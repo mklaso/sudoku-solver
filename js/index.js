0956d4aa781f58a0e979d6b9f8241739eadfa4db
mklaso <mklasninovski@gmail.com>
2020-10-18 19:45:21 -0400

    naming convention change for clarity.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
 // main file - handles everything using mvc
 
-let sudokuGrid = document.querySelector(".sudoku-grid");
-
-console.log(sudokuGrid);
+let sudokuGrid = document.querySelector(".sudoku__grid");
 
 const renderInnerGrids = () => {
   const markup = `
-    <div class="inner-grid-cell">
-        <div class="cell cell-1"></div>
-        <div class="cell cell-2">2</div>
-        <div class="cell cell-3">3</div>
-        <div class="cell cell-4">4</div>
-        <div class="cell cell-5">5</div>
-        <div class="cell cell-6">6</div>
-        <div class="cell cell-7">7</div>
-        <div class="cell cell-8">8</div>
-        <div class="cell cell-9">9</div>
+    <div class="inner__grid">
+        <div class="inner__grid--cell"></div>
+        <div class="inner__grid--cell">2</div>
+        <div class="inner__grid--cell">3</div>
+        <div class="inner__grid--cell">4</div>
+        <div class="inner__grid--cell"></div>
+        <div class="inner__grid--cell">6</div>
+        <div class="inner__grid--cell">7</div>
+        <div class="inner__grid--cell">8</div>
+        <div class="inner__grid--cell"></div>
     </div>
     `;
 
